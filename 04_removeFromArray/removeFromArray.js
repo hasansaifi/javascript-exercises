790bd8b92e447b72cca47d217864fecacf85a1ea
hasansaifi <ibnehasan789@gmail.com>
2024-07-28 22:19:11 +0300

    Completed 3rd test of 4th JS exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,18 +2,30 @@ const removeFromArray = function(...theArgs) {
     let initArray = theArgs;
     let array = initArray.shift();
     let i = initArray.length;
+
     
-    for (const arg of theArgs) {
+    for (const arg of array) {
         i--;
-        let remove = initArray[i];
-        let rmIndex = array.indexOf(remove);
-        array.splice(rmIndex, 1)
+        let removeVal = initArray[i];
+
+        let rmIndex = array.indexOf(removeVal);
+
+        const itemCounter = (value, index) => {
+            return value.filter((x) => x == index).length;
+        };
+        
+        let occurance = itemCounter(array, removeVal);
+       
+        for(occurance; occurance > 0; occurance--){
+            array.splice(rmIndex, 1)
+        }
+          
     }
 
-
+    console.log(array);
     return array;
 }; 
 
-removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
 // Do not edit below this line
 module.exports = removeFromArray;
