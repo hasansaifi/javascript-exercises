4f323d1cf071b6feda3708370d91a318864136e0
hasansaifi <ibnehasan789@gmail.com>
2024-07-28 23:05:39 +0300

    Solved all 8 tests from RemoveFromArray.
    But my solution is way bigger than what solution
    is provided.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,31 +1,39 @@
 const removeFromArray = function(...theArgs) {
+    //Storing exact value of received array to manipulate it
     let initArray = theArgs;
+    //To remove initial array from argument
     let array = initArray.shift();
     let i = initArray.length;
 
-    
+    //Main loop to remove requested items
     for (const arg of theArgs) {
         i--;
         let removeVal = initArray[i];
+        console.log(removeVal);
 
         let rmIndex = array.indexOf(removeVal);
 
+        //To check if an item occured more than once
         const itemCounter = (value, index) => {
             return value.filter((x) => x == index).length;
         };
-        
         let occurance = itemCounter(array, removeVal);
        
-        for(occurance; occurance > 0; occurance--){
+        //To check if items are of same type.
+        if(typeof initArray[i] === typeof array[i]){
+            //Loop to remove items that occured more than once. Also support single items.
+            for(occurance; occurance > 0; occurance--){
             array.splice(rmIndex, 1)
         }
+        }
+        
           
     }
 
-    console.log(array);
+    
     return array;
 }; 
 
-removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+//console.log("Sent Array: " + removeFromArray([1, 2, 3], "1", 3));
 // Do not edit below this line
 module.exports = removeFromArray;
