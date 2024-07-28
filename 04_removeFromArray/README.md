03ea3be90ec53be16882ee90c7b2530263145f68
hasansaifi <ibnehasan789@gmail.com>
2024-07-28 20:16:13 +0300

    Passed 2nd test of 4th JS exercise

--- 04_removeFromArray/README.md
+++ 04_removeFromArray/README.md
@@ -1,5 +1,5 @@
 # Exercise 04 - removeFromArray
-
+ 
 Implement a function that takes an array and some other arguments then removes the other arguments from that array:
 
 ```javascript
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(...theArgs) {
+    let initArray = theArgs;
+    let array = initArray.shift();
+    let i = initArray.length;
+    
+    for (const arg of theArgs) {
+        i--;
+        let remove = initArray[i];
+        let rmIndex = array.indexOf(remove);
+        array.splice(rmIndex, 1)
+    }
 
+
+    return array;
+}; 
+
+removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
