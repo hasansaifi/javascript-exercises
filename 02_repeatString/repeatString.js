cddae5270bcd18c706da45dd1d6fc69e6602219b
hasansaifi <ibnehasan789@gmail.com>
2024-07-28 18:59:14 +0300

    Completed 2nd JS exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, repeatCount) {
+    let finalString = "";
+    let i = repeatCount;
 
+    if (i >= 0) {
+        for (i; i === 0, i--;) {
+            finalString += string;
+        }
+    } else { finalString = "ERROR" }
+
+    return finalString;
 };
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString; 
