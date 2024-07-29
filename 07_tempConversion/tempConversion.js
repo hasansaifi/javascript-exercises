27eb96c485800782986ea685a44da6510d3411c1
hasansaifi <ibnehasan789@gmail.com>
2024-07-29 12:52:51 +0300

    Passed all test of 6th exercise of JS

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
 const convertToCelsius = function(tempF) {
+  // Convert f to c
   let c =  5/9 * (tempF-32);
+  //Reduce decimal to 1 and convert string to float
   c = parseFloat(c.toFixed(1));
   return c;
 }; 
 
 const convertToFahrenheit = function(tempC) {
+  // Convert c to f
+  let f = (tempC * 9/5) + 32;
+  //Reduce decimal to 1 and convert string to float
+  f = parseFloat(f.toFixed(1));
+  return f;
 };
 
 // Do not edit below this line
