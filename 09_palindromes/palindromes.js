e08160780b9ad648bc9c3292c4c70aa2ab55d259
hasansaifi <ibnehasan789@gmail.com>
2024-07-31 00:06:27 +0300

    Passed all 8 tests. Learned about regex.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (string) {
     string = string.toLowerCase();
-    string = string.replace(/[^a-zA-Z ]/g, "")
+    string = string.replace(/[^a-zA-Z0-9 ]/g, "")
     string = string.replace(/\s/g, "")
     const reversedString = string.split("").reduce((acc, char) => char + acc, "");
     console.log(reversedString);
