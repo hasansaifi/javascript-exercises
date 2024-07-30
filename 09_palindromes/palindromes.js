27deba0d5578d75c2d046d7eb72c1615b5021438
hasansaifi <ibnehasan789@gmail.com>
2024-07-30 23:57:48 +0300

    Passed 7 out of 8 tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
 const palindromes = function (string) {
     string = string.toLowerCase();
     string = string.replace(/[^a-zA-Z ]/g, "")
+    string = string.replace(/\s/g, "")
     const reversedString = string.split("").reduce((acc, char) => char + acc, "");
     console.log(reversedString);
     if(string == reversedString){
