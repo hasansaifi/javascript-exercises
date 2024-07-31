3b0489cbf4672d9142bf74b88277f09b23bc898e
hasansaifi <ibnehasan789@gmail.com>
2024-08-01 02:06:31 +0300

    Passed all 3 tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,11 +1,17 @@
 const findTheOldest = function(people){
+    const date = new Date();
+   
 
-    const oldest = people.sort(function(a, b) {
+    const oldest = people.sort(function(a, b) {  
+        if (!("yearOfDeath" in b)){
+            b.yearOfDeath = date.getFullYear();
+        }
         const lastPerson = a.yearOfDeath - a.yearOfBirth;
         const nextPerson = b.yearOfDeath - b.yearOfBirth;
         return lastPerson > nextPerson ? -1 : 1;
     });
 
+    //console.table(oldest);
     return oldest[0];
     
 };
